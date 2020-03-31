c7a7d45b77d3ef63fc53fd92e50cd5de24ab5fa9
Alex Roetter <aroetter@users.noreply.github.com>
2020-03-31 11:00:41 -0600

    Moved everything to a /todo top-level URI

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-
+import routes
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test.db"
@@ -18,7 +18,7 @@ class Todo(db.Model):
 	def __repr__(self):
 		return "<Task %r>" % self.id
 
-@app.route("/", methods=["POST", "GET"])
+@app.route(routes.TODO_APP_BASEURL, methods=["POST", "GET"])
 def index():
 	if request.method == "POST":
 		# Grab the task and put it in the DB
@@ -27,36 +27,36 @@ def index():
 		try:
 			db.session.add(new_task)
 			db.session.commit()
-			return redirect("/")
-		except:
-			return "There was an issue adding your task!"
+			return redirect(routes.TODO_APP_BASEURL)
+		except Exception as ex:
+			return "There was an issue adding your task!" + repr(ex)
 	else:
 		# Show all the tasks
 		tasks = Todo.query.order_by(Todo.date_created).all()
-		return render_template("index.html", tasks=tasks)
+		return render_template("index.html", tasks=tasks, base_url=routes.TODO_APP_BASEURL)
 
-@app.route("/delete/<int:id>")
+@app.route(routes.TODO_APP_BASEURL + "/delete/<int:id>")
 def delete(id):
 	task_to_delete = Todo.query.get_or_404(id)
 	try:
 		db.session.delete(task_to_delete)
 		db.session.commit()
-		return redirect("/")
-	except:
-		return "Couldn't delete this task from the DB!"
+		return redirect(routes.TODO_APP_BASEURL)
+	except Exception as ex:
+		return "Couldn't delete this task from the DB!" + repr(ex)
 
-@app.route("/update/<int:id>", methods=["GET", "POST"])
+@app.route(routes.TODO_APP_BASEURL + "/update/<int:id>", methods=["GET", "POST"])
 def update(id):
 	task = Todo.query.get_or_404(id)
 	if request.method == "POST":
 		task.content = request.form['content']
 		try:
 			db.session.commit()
-			return redirect("/")
-		except:
-			return "Couldn't update this task in the DB!"
+			return redirect(routes.TODO_APP_BASEURL)
+		except Exception as ex:
+			return "Couldn't update this task in the DB!" + repr(ex)
 	else:
-		return render_template("update.html", task=task)
+		return render_template("update.html", task=task, base_url=routes.TODO_APP_BASEURL)
 	return "..."
 
 if __name__ == "__main__":
